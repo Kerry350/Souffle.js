dbd386b1f809892b008fa6fa978518d98e83b051
Kerry Gallagher <k.gallagher.05@gmail.com>
2013-11-23 00:41:04 +0000

    Initial commit

---
+++ souffle.js
@@ -0,0 +1,170 @@
+(function(root) {
+
+  /* 
+
+    Two APIs, one for validating entire objects against a set of rules. Another for direct validations.
+
+    Rule object: 
+
+      Souffle({
+        name: 'Magical Donkey', 
+        age: 15,
+        owner: {
+          firstName: 'Magic', 
+          lastName: 'Dave'
+        }
+      },
+
+      {
+        name: {
+          isString: true, 
+          minLength: 10, 
+          maxLength: 20
+        },
+
+        age: {
+          isNotBlank: true
+        },
+
+        owner: {
+          firstName: {
+            isNotBlank: true
+          },
+          lastName: {
+            isNotBlank: true
+          }
+        }
+      }); // Returns an errors array
+
+      Souffle('Donkey').isString().length(5, 10).exec();
+  */
+
+  validations: {
+    isNumber: {
+      fn: function() {
+
+      },
+      error: function(val) {
+
+      }
+    },
+
+    isArray: {
+      fn: function() {
+
+      },
+      error: function(val) {
+
+      }
+    },
+
+    isString: {
+      fn: function() {
+
+      },
+      error: function(val) {
+
+      }
+    },
+
+    isEmail: {
+      fn: function() {
+
+      },
+      error: function(val) {
+
+      }
+    },
+
+    isURL: {
+      fn: function() {
+
+      },
+      error: function(val) {
+
+      }
+    },
+
+    matches: {
+      fn: function() {
+
+      },
+      error: function(val) {
+
+      }
+    },
+
+    isNotBlank: {
+      fn: function() {
+
+      },
+      error: function(val) {
+
+      }
+    },
+
+    length: {
+      fn: function() {
+
+      },
+      error: function(val) {
+
+      }
+    },
+
+    minLength: {
+      fn: function() {
+
+      },
+      error: function(val) {
+
+      }
+    },
+
+    maxLength: {
+      fn: function() {
+
+      },
+      error: function(val) {
+
+      }
+    },
+
+    minKeys: {
+      fn: function() {
+
+      },
+      error: function(val) {
+
+      }
+    }
+  };
+
+
+  var Souffle = function(values, rules) {
+    if (!values && !rules) {
+      throw new Error('Please either provide an object and corresponding ruleset, or a value');
+    }
+
+    if ((values && typeof values === 'object') && (rules && typeof rules === 'object')) {
+      this.errors = [];
+      this.validateRuleset(values, rules, this.errors);
+      return this.errors;
+    }
+
+    else {
+      this.errors = [];
+      this.value = values;
+      return this;
+    }
+  };
+
+  Souffle.prototype = {
+    exec: function() {
+      // return this errors
+    }
+  };
+
+  root.Souffle = Souffle;
+
+})(window);
